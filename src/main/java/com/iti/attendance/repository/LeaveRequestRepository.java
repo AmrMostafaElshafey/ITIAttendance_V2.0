7e4acaa46227b2038f2f16b2c491b59e6dded62f
AmrMostafaElshafey <127788289+AmrMostafaElshafey@users.noreply.github.com>
2025-11-27 20:01:53 +0200

    Fix leave request navigation by role

--- src/main/java/com/iti/attendance/repository/LeaveRequestRepository.java
+++ src/main/java/com/iti/attendance/repository/LeaveRequestRepository.java
@@ -3,6 +3,9 @@ package com.iti.attendance.repository;
 import com.iti.attendance.model.LeaveRequest;
 import com.iti.attendance.model.RequestType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
 import java.util.List;
 
 public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long> {
@@ -13,4 +16,10 @@ public interface LeaveRequestRepository extends JpaRepository<LeaveRequest, Long
     List<LeaveRequest> findByEmployeeIdAndDeletedFalseAndType(Long employeeId, RequestType type);
 
     List<LeaveRequest> findByDeletedFalseAndType(RequestType type);
+
+    @Query("SELECT lr FROM LeaveRequest lr WHERE lr.employee.id = :employeeId AND lr.deleted = false AND (lr.type = :type OR (:includeNullType = true AND lr.type IS NULL))")
+    List<LeaveRequest> findByEmployeeIdAndDeletedFalseAndTypeOrNull(@Param("employeeId") Long employeeId, @Param("type") RequestType type, @Param("includeNullType") boolean includeNullType);
+
+    @Query("SELECT lr FROM LeaveRequest lr WHERE lr.deleted = false AND (lr.type = :type OR (:includeNullType = true AND lr.type IS NULL))")
+    List<LeaveRequest> findByDeletedFalseAndTypeOrNull(@Param("type") RequestType type, @Param("includeNullType") boolean includeNullType);
 }
